056b59fbbdb9736e79d133932e0c078b54bdb0c1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-17 22:33:45 -0400

    Add files via upload

---
+++ playerctl-appimage.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+APP=playerctl
+APPDIR="$APP".AppDir
+REPO="https://github.com/altdesktop/playerctl.git"
+
+# CREATE DIRECTORIES
+if [ -z "$APP" ]; then exit 1; fi
+mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
+
+# DOWNLOAD AND BUILD PLAYERCTL STATICALLY
+CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
+CFLAGS='-static -O3' 
+LDFLAGS="-static"
+
+git clone --recursive "$REPO" && cd playerctl \
+&& meson setup build -Dprefix="$CURRENTDIR" -Ddefault_library=static -Dgtk-doc=false -Dintrospection=false \
+&& ninja -C build && ninja -C build install && cd .. && rm -rf ./playerctl ./include \
+&& sed -i 's#Exec=.*#Exec=playerctld daemon#g' ./share/dbus-1/services/org.mpris.MediaPlayer2.playerctld.service || exit 1
+
+# AppRun
+cat >> ./AppRun << 'EOF'
+#!/bin/sh
+CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+"$CURRENTDIR"/bin/playerctl "$@"
+EOF
+chmod a+x ./AppRun
+
+APPVERSION=$(./AppRun --version | awk '{print $1}')
+
+# Desktop
+cat >> ./"$APP.desktop" << 'EOF'
+[Desktop Entry]
+Type=Application
+Name=playerctl
+Icon=playerctl
+Exec=playerctl
+Categories=System
+Hidden=true
+EOF
+
+# Icon
+touch ./"$APP".png && ln -s ./"$APP".png ./.DirIcon # Does playerctl have an official icon?
+
+# MAKE APPIMAGE
+cd ..
+APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
+wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool
+
+# Do the thing!
+ARCH=x86_64 VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR"
+ls ./*.AppImage || { echo "appimagetool failed to make the appimage"; exit 1; }
+if [ -z "$APP" ]; then exit 1; fi # Being extra safe lol
+mv ./*.AppImage .. && cd .. && rm -rf ./"$APP"
+echo "All Done!"
