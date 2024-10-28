ea544390b1afb3501a868112d955379d5a1c8c38
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-28 00:19:52 -0400

    refactor and deploy all libs

--- playerctl-appimage.sh
+++ playerctl-appimage.sh
@@ -1,78 +1,73 @@
 #!/bin/sh
 
+set -u
+
 APP=playerctl
 APPDIR="$APP".AppDir
 REPO="https://github.com/altdesktop/playerctl.git"
+export ARCH="$(uname -m)"
+export APPIMAGE_EXTRACT_AND_RUN=1
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+SHARUN="https://bin.ajam.dev/$ARCH/sharun"
 
 # CREATE DIRECTORIES
-if [ -z "$APP" ]; then exit 1; fi
 mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
 # DOWNLOAD AND BUILD PLAYERCTL STATICALLY
 CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
-CFLAGS='-static -O3' 
+CFLAGS='-static -O3'
 LDFLAGS="-static"
 
 git clone --recursive "$REPO" && cd playerctl \
-&& meson setup build -Dprefix="$CURRENTDIR" -Ddefault_library=static -Dgtk-doc=false -Dintrospection=false \
-&& ninja -C build && ninja -C build install && cd .. && rm -rf ./playerctl ./include \
-&& sed -i 's#Exec=.*#Exec=playerctl daemon#g' ./share/dbus-1/services/org.mpris.MediaPlayer2.playerctld.service || exit 1
-ldd ./bin/playerctl
+	&& meson setup build -Dprefix="$CURRENTDIR" -Dgtk-doc=false -Dintrospection=false \
+	&& ninja -C build && ninja -C build install && cd .. && rm -rf ./playerctl ./include || exit 1
+sed -i 's#Exec=.*#Exec=playerctl daemon#g' ./share/dbus-1/services/org.mpris.MediaPlayer2.playerctld.service || exit 1
+
+# ADD LIBRARIES
+mkdir -p ./shared/lib && mv ./lib/*/* ./shared/lib || exit 1
+wget "$LIB4BN" -O ./lib4bin && wget "$SHARUN" -O ./sharun || exit 1
+chmod +x ./lib4bin ./sharun
+HARD_LINKS=1 ./lib4bin ./bin/* && rm -f ./lib4bin || exit 1
 
 # AppRun
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
 DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
-
-_install_daemon() {
-	DAEMON="$(find "$CURRENTDIR" -type f -name 'org*.service' -print -quit)"
-	mkdir -p "$DATADIR"/dbus-1/services || exit 1
-	cp -n "$DAEMON" "$DATADIR"/dbus-1/services || exit 1
-	echo "Dbus service installed at $DATADIR/dbus-1/services"
-	exit 0
-}
-_run_daemon() {
-	if [ ! -f "$DATADIR"/dbus-1/services/*playerctld* ]; then
-		echo 'You need to run "--install-daemon" to install the dbus service'
-		exit 1
-	fi
-	exec "$CURRENTDIR"/bin/playerctld
-}
-
-BIN="$ARGV0"
+BIN="${ARGV0#./}"
+DAEMON="$(find "$CURRENTDIR" -type f -name 'org*.service' -print -quit 2>/dev/null)"
+daemon_name="$(basename "$DAEMON")"
+export PATH="$CURRENTDIR/bin:$PATH"
 unset ARGV0
-case "$BIN" in
-	'playerctl'|'playerctld')
-		exec "$CURRENTDIR/bin/$BIN" "$@"
-		;;
-	'')
-		"$CURRENTDIR"/bin/playerctl "$@"
-		if [ -z "$1" ]; then
-			echo "AppImage commands:"
-			echo " \"--install-daemon\" Installs dbus service in $DATADIR"
-			echo " \"--daemon\"         Starts playerctld daemon"
-			echo "You can also symlink the appimage with the name playerctld"
-			echo "to start the daemon by launching that symlink"
-		fi
-		;;
 
-	*)
-		case "$1" in
-		'--daemon')
-			_run_daemon
-			;;
-		'--install-daemon')
-			_install_daemon
-			;;
+_playerctld () {
+	if [ ! -e "$DATADIR/dbus-1/services/$daemon_name" ]; then
+		mkdir -p "$DATADIR"/dbus-1/services || exit 1
+		cp -n "$DAEMON" "$DATADIR"/dbus-1/services || exit 1
+		echo "Dbus service installed at $DATADIR/dbus-1/services/$daemon_name"
+	fi
+	exec "$CURRENTDIR"/bin/playerctld "$@"
+}
 
-		esac
-	;;
-esac
+if [ "$1" = "--daemon" ]; then
+	shift
+	_playerctld "$@"
+elif [ "$BIN" = "playerctld" ]; then
+	_playerctld "$@"
+else
+	exec "$CURRENTDIR"/bin/playerctl "$@"
+fi
+if [ -z "$1" ]; then
+	echo "AppImage commands:"
+	echo " \"--daemon\"         Starts playerctld daemon"
+	echo "You can also symlink the appimage with the name playerctld"
+	echo "to start the daemon by launching that symlink"
+fi
 EOF
 chmod a+x ./AppRun
 
-APPVERSION=$(./AppRun --version | awk '{print $1}')
+VERSION=$(./bin/playerctl --version | awk 'FNR==1 {print $1; exit}')
 
 # Desktop
 cat >> ./"$APP.desktop" << 'EOF'
@@ -90,12 +85,13 @@ touch ./"$APP".png && ln -s ./"$APP".png ./.DirIcon # Does playerctl have an off
 
 # MAKE APPIMAGE
 cd ..
-APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
-wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool
+wget -q "$APPIMAGETOOL" -O ./appimagetool || exit 1
+chmod +x ./appimagetool
 
 # Do the thing!
-ARCH=x86_64 VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR"
-ls ./*.AppImage || { echo "appimagetool failed to make the appimage"; exit 1; }
-if [ -z "$APP" ]; then exit 1; fi # Being extra safe lol
-mv ./*.AppImage .. && cd .. && rm -rf ./"$APP"
+echo "Making appimage"
+./appimagetool --comp zstd \
+	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
+	./"$APP".AppDir "$APP"-"$VERSION"-"$ARCH".AppImage
+mv ./*.AppImage .. && cd .. && rm -rf ./"$APP" || exit 1
 echo "All Done!"
